e7ef085feb43e2f5ed79d640a55ae61900446a05
Chris <christophersingleton99@gmail.com>
2025-11-11 15:46:37 -0500

    v 0.0
    files created

---
+++ Team6_CmpSc33_Functions/Team6_CmpSc33_Functions.cpp
@@ -0,0 +1,10 @@
+// Team6_CmpSc33_Functions.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnTeam6CmpSc33Functions()
+{
+}
