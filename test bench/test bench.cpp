1c02362c807c2a4f94f6107a44c75d074de85449
Chris <christophersingleton99@gmail.com>
2025-11-11 20:42:52 -0500

    v 0.1
    built functions to detect if a 5-card hand is a flush or a straight, created a test environment to make sure the functions work as expected

--- test bench/test bench.cpp
+++ test bench/test bench.cpp
@@ -3,8 +3,6 @@
 //
 
 
-#include "pch.h"
-#include "framework.h"
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -21,8 +19,8 @@ std::vector<std::vector<std::string>> handDecode(std::vector<int> hand)
 	for (int i = 0; i < hand.size(); i++) //loop through the hand
 	{
 		std::vector<std::string> handHere;
-		handHere.push_back(RANKS.at((hand.at(i)-1) % 13)); // for each element in the hand, set the rank to the modulus of the number and 13
-		handHere.push_back(SUITS.at((hand.at(i)-1) / 13)); // for each element in the hand, set the suit to the result of integer division number/13
+		handHere.push_back(RANKS.at((hand.at(i) - 1) % 13)); // for each element in the hand, set the rank to the modulus of the number and 13
+		handHere.push_back(SUITS.at((hand.at(i) - 1) / 13)); // for each element in the hand, set the suit to the result of integer division number/13
 		handMatrix.push_back(handHere);
 	}
 	return handMatrix;
@@ -84,4 +82,55 @@ int handIncludesMultiples(std::vector<int> hand)
 	sort(hand.begin(), hand.end());
 
 	return 0; // return 0 if no, 1 if two-of-a-kind, 2 if three-of-a-kind, 3 if four-of-a-kind 
+}
+
+
+
+
+
+
+
+//------------------------------------------------
+
+
+int main()
+{
+	int cardCount = 0;
+	const std::vector<int> deck = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52};
+
+	const std::vector<int> handone = { 1,2,3,4,5};
+	const std::vector<int> handtwo = { 11,12,13,14,15 };
+	const std::vector<int> handthree = { 1,6,7,13,50 };
+	const std::vector<int> handfour = { 20,30,40,50,52 };
+
+	//isHandFlush(deck);
+	//std::cout << isHandFlush(deck) << "\n";
+
+	std::cout << isHandFlush(handone) << "\n";
+
+	std::cout << isHandFlush(handtwo) << "\n";
+
+	std::cout << isHandFlush(handthree) << "\n";
+
+	std::cout << isHandFlush(handfour) << "\n";
+
+
+	std::cout << isHandStraight(handone) << "\n";
+
+	std::cout << isHandStraight(handtwo) << "\n";
+
+	std::cout << isHandStraight(handthree) << "\n";
+
+	std::cout << isHandStraight(handfour) << "\n";
+
+
+	/*for (int i = 0; i < 4; i++) // prints out the original contents of the deck
+	{
+		for (int j = 0; j < 13; j++)
+		{
+			cardCount++;
+			printf("%2d ", cardCount);
+		}
+		std::cout << "   <-- " << SUITS[i] << "\n";
+	}*/
 }
